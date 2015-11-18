41b031c82f3cd9964fbd8f00acf8a47fa94188ba
Jack Morgan <ictjackmorgz@gmail.com>
2015-11-18 02:20:17 +0000

    Updated bottle-boat code, added TinyGPS++ library to repository
    Bottle-boat code: moved my funcs underneath Arduino funcs (setup & loop)
    and improved commenting. | New library: added TinyGPS++

--- BottleBoat-HydroCraft/BottleBoat-HydroCraft.ino
+++ BottleBoat-HydroCraft/BottleBoat-HydroCraft.ino
@@ -1,7 +1,9 @@
+#include <TinyGPS++.h>
 #include <Wire.h>
 #include <I2Cdev.h>
 #include <HMC5883L.h>
 #include <Servo.h>
+
 
 /*
 Program for the bottle-boat designed by group HydroCraft, under robotics team Aber Sailbot
@@ -10,6 +12,8 @@ at Aberystwyth University. Aber SailBot website available at: http://abersailbot
 Created October 28th by Jack Morgan (jam30@aber.ac.uk)
 Project repository available at: https://github.com/abersailbot/bottleboat-HydroCraft
 */
+
+// Latest change: moved my funcs beneath Arduino funcs (setup & loop) and improved commenting
 
 HMC5883L msg;
 int16_t mx, my, mz;
@@ -20,10 +24,13 @@ int pos = 0;
 
 void setup() {
   Serial.begin(9600);
-  rudder.attach(5);
-  pinMode(3,OUTPUT);
-  analogWrite(3, 125);
-  rudder.write(60);
+  pinMode(3,OUTPUT); rudder.attach(5);
+  analogWrite(3, 125); rudder.write(60);
+}
+
+void loop() {
+  readCompass();
+  delay(200);
 }
 
 void readCompass() {
@@ -32,27 +39,18 @@ void readCompass() {
   msg.getHeading(&mx, &my, &mz);
 
   // report getHeading results for debugging purposes
-  Serial.print("X:");
-  Serial.println(mx);
-  Serial.print("Y :");
-  Serial.println(my);
-  Serial.print("Heading: ");
-  Serial.println(heading());
+  Serial.print("X:"); Serial.println(mx);
+  Serial.print("Y :"); Serial.println(my);
+  Serial.print("Heading: "); Serial.println(heading());
 }
 
 float heading() {
-
-  // read compass and check for bad data
-  float heading = atan2(my, mx); // 0 = north
+  // read compass and correct for bad data, then return result
+  // 0 = north
+  float heading = atan2(my, mx);
 
   if (heading < 0)
     heading += 2 * M_PI;
 
   return heading * 180 / M_PI;
 }
-
-void loop() {
-  readCompass();
-  delay(200);
-}
-
